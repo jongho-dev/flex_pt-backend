e29a66c9ed85139882d8b869934a4c485641a27c
JongHoPark <qkrwhdgh126@gmail.com>
2024-02-07 15:52:06 +0900

    feat: mongodb 연결

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require("express");
+const connectDB = require("./db");
 var cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
+const lst = {
+  id: 1,
+  text: "할일",
+};
+
 app.get("/", (req, res) => {
   res.send("hello node");
 });
 
-app.get("/dog", (req, res) => {
-  res.send("hello dog");
+app.get("/todo", (req, res) => {
+  res.json(lst);
 });
+
+app.get("/register");
 
 app.get("/user/:id", (req, res) => {
   const q = req.params;
   res.send(q);
 });
+
+connectDB();
 
 app.listen(5000, () => console.log("5000번 포트에서 대기중"));
