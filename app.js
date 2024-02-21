90f69b4164bfc2c3a823316378a7bfdbf4b2fec9
JongHoPark <qkrwhdgh126@gmail.com>
2024-02-21 16:14:05 +0900

    feat: 로그인 구현

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { connectDB, insertData, findData, idCheck } = require("./db");
+const { connectDB, createUser, Login, idCheck } = require("./db");
 var cors = require("cors");
 require("dotenv").config();
 
@@ -9,38 +9,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("hello node");
-});
-
-app.get("/cat", (req, res) => {
-  res.send("hello cat");
-});
-
 connectDB();
 
-app.get("/user", (req, res) => {
-  insertData();
-  res.json("성공");
-});
-
 // 로그인
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  findData();
+app.post("/login", async (req, res) => {
+  const rst = await Login(req.body["id"], req.body["pw"]);
+  res.send(rst);
 });
 
-// 회원가입 관련
-//// 아이디 중복체크
+// 회원가입 - 아이디 중복 체크
 app.post("/signupidcheck", async (req, res) => {
   const result = await req.body["id"];
   const rst = await idCheck(result);
   res.send(rst);
 });
 
-//// 회원가입 완료
+// 회원가입 - 유저 생성
 app.post("/signupdone", (req, res) => {
-  insertData(req.body);
+  createUser(req.body);
 });
 
 app.listen(4000, () => console.log("4000번 포트에서 대기중"));
--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const dbName = "flexpt";
 const client = new MongoClient(uri);
 var db;
 
+// DB 연결
 const connectDB = async () => {
   try {
     await client.connect();
@@ -16,7 +17,25 @@ const connectDB = async () => {
   }
 };
 
-const insertData = async (data) => {
+// 로그인
+const Login = async (id, pw) => {
+  const usersCollection = client.db("flexpt").collection("users");
+  var cursor = await usersCollection.findOne({
+    id: id,
+    pw: pw,
+  });
+  return cursor != null;
+};
+
+// 회원가입 - 아이디 중복 체크
+const idCheck = async (id) => {
+  const usersCollection = client.db("flexpt").collection("users");
+  var cursor = await usersCollection.findOne({ id: id });
+  return cursor != null;
+};
+
+// 회원가입 - 유저 생성
+const createUser = async (data) => {
   try {
     const usersCollection = client.db("flexpt").collection("users");
     const result = await usersCollection.insertOne(data);
@@ -26,16 +45,4 @@ const insertData = async (data) => {
   }
 };
 
-const findData = async (id, pw) => {
-  const usersCollection = client.db("flexpt").collection("users");
-  var cursor = await usersCollection.findOne({ id: id, password: pw });
-  console.log(cursor);
-};
-
-const idCheck = async (id) => {
-  const usersCollection = client.db("flexpt").collection("users");
-  var cursor = await usersCollection.findOne({ id: id });
-  return cursor != null;
-};
-
-module.exports = { connectDB, insertData, findData, idCheck };
+module.exports = { connectDB, Login, createUser, idCheck };
